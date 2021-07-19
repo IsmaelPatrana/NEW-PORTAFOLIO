78ce6858f9062547b9c809668dcdb6905216a25c
IsmaelBack <72406061+IsmaelPatrana@users.noreply.github.com>
2021-07-19 11:33:44 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal('.img-ct', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('.texto-hd', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px'
+});
+sr.reveal('.texto-hd_2', {
+    duration: 3000,
+    origin: 'left',
+    distance: '100px'
+});
+sr.reveal('.texto-hd_3', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('.icon-header', {
+    duration: 3000,
+    origin: 'left',
+    distance: '100px'
+});
+/*sobre mi*/
+sr.reveal('.sobremi-1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '400px'
+});
+
+
+
